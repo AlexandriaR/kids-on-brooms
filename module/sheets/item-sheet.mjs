b7dee7d59a07a5c6cf524df9f16ef277a86b8834
Josiah Bradbury <herrozerro@gmail.com>
2021-08-11 07:00:23 -0500

    updated to latest version of generator

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -18,30 +18,35 @@ export class KidsOnBroomsItemSheet extends ItemSheet {
   get template() {
     const path = "systems/kidsonbrooms/templates/item";
     // Return a single sheet for all item types.
-    return `${path}/item-sheet.html`;
+    // return `${path}/item-sheet.html`;
+
     // Alternatively, you could use the following return statement to do a
     // unique item sheet by type, like `weapon-sheet.html`.
-
-    // return `${path}/${this.item.data.type}-sheet.html`;
+    return `${path}/item-${this.item.data.type}-sheet.html`;
   }
 
   /* -------------------------------------------- */
 
   /** @override */
   getData() {
-    const data = super.getData();
-    return data;
-  }
+    // Retrieve base data structure.
+    const context = super.getData();
 
-  /* -------------------------------------------- */
+    // Use a safe clone of the item data for further operations.
+    const itemData = context.item.data;
 
-  /** @override */
-  setPosition(options = {}) {
-    const position = super.setPosition(options);
-    const sheetBody = this.element.find(".sheet-body");
-    const bodyHeight = position.height - 192;
-    sheetBody.css("height", bodyHeight);
-    return position;
+    // Retrieve the roll data for TinyMCE editors.
+    context.rollData = {};
+    let actor = this.object?.parent ?? null;
+    if (actor) {
+      context.rollData = actor.getRollData();
+    }
+
+    // Add the actor's data to context.data for easier access, as well as flags.
+    context.data = itemData.data;
+    context.flags = itemData.flags;
+
+    return context;
   }
 
   /* -------------------------------------------- */
@@ -51,7 +56,7 @@ export class KidsOnBroomsItemSheet extends ItemSheet {
     super.activateListeners(html);
 
     // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
+    if (!this.isEditable) return;
 
     // Roll handlers, click handlers, etc. would go here.
   }
